aa79dfdc24e1fee0c41491814d04b709d7e7f7ac
Archford Nhanga <archfordnhanga@gmail.com>
2025-06-19 20:42:36 +0200

    semester functions

--- controllers/semester.js
+++ controllers/semester.js
@@ -1,50 +1,49 @@
 const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
 
-// Get all semesters
+/**
+ * Get all semesters.
+ * @swagger tags: ['semesters']
+ */
 const getAll = async (req, res) => {
-  //#swagger.tags=['semesters']
   try {
-    const result = await mongodb
-      .getDatabase()
-      .collection('semester')
-      .find();
+    const result = await mongodb.getDatabase().collection('semester').find();
     const semesters = await result.toArray();
     res.status(200).json(semesters);
   } catch (err) {
     console.error(err);
-    res
-      .status(500)
-      .json({ message: err.message || 'Error retrieving semesters' });
+    res.status(500).json({ message: err.message || 'Error retrieving semesters' });
   }
 };
 
-// Get a single semester
+/**
+ * Get a single semester by ID.
+ * @swagger tags: ['semesters']
+ */
 const getSingle = async (req, res) => {
-  //#swagger.tags=['semesters']
   try {
     const semesterId = new ObjectId(req.params.id);
-    const result = await mongodb
+    const semester = await mongodb
       .getDatabase()
       .collection('semester')
-      .find({ _id: semesterId });
-    const semester = await result.toArray();
-    if (semester.length > 0) {
-      res.status(200).json(semester[0]);
+      .findOne({ _id: semesterId });
+
+    if (semester) {
+      res.status(200).json(semester);
     } else {
       res.status(404).json({ message: 'Semester not found' });
     }
   } catch (err) {
     console.error(err);
-    res
-      .status(500)
-      .json({ message: err.message || 'Error retrieving semester' });
+    res.status(500).json({ message: err.message || 'Error retrieving semester' });
   }
 };
 
-// Create a new semester
+/**
+ * Create a new semester.
+ * @swagger tags: ['semesters']
+ */
 const createSemester = async (req, res) => {
-  //#swagger.tags=['semesters']
   try {
     const semester = {
       year: req.body.year,
@@ -52,10 +51,12 @@ const createSemester = async (req, res) => {
       semesterStart: req.body.semesterStart,
       semesterEnd: req.body.semesterEnd,
     };
+
     const response = await mongodb
       .getDatabase()
       .collection('semester')
       .insertOne(semester);
+
     if (response.acknowledged) {
       res.status(201).json(response);
     } else {
@@ -67,9 +68,11 @@ const createSemester = async (req, res) => {
   }
 };
 
-// Update a semester
+/**
+ * Update a semester by ID.
+ * @swagger tags: ['semesters']
+ */
 const updateSemester = async (req, res) => {
-  //#swagger.tags=['semesters']
   try {
     const semesterId = new ObjectId(req.params.id);
     const semester = {
@@ -78,14 +81,16 @@ const updateSemester = async (req, res) => {
       semesterStart: req.body.semesterStart,
       semesterEnd: req.body.semesterEnd,
     };
+
     const response = await mongodb
       .getDatabase()
       .collection('semester')
       .replaceOne({ _id: semesterId }, semester);
+
     if (response.modifiedCount > 0) {
       res.status(204).send();
     } else {
-      res.status(404).json({ message: 'semester not found' });
+      res.status(404).json({ message: 'Semester not found' });
     }
   } catch (err) {
     console.error(err);
@@ -93,19 +98,22 @@ const updateSemester = async (req, res) => {
   }
 };
 
-// Delete semester
+/**
+ * Delete a semester by ID.
+ * @swagger tags: ['semesters']
+ */
 const deleteSemester = async (req, res) => {
-  //#swagger.tags=['semesters']
   try {
     const semesterId = new ObjectId(req.params.id);
     const response = await mongodb
       .getDatabase()
       .collection('semester')
       .deleteOne({ _id: semesterId });
+
     if (response.deletedCount > 0) {
-      res.status(200).json({ message: 'semester deleted' });
+      res.status(200).json({ message: 'Semester deleted successfully' });
     } else {
-      res.status(404).json({ message: 'semester not found' });
+      res.status(404).json({ message: 'Semester not found' });
     }
   } catch (err) {
     console.error(err);
